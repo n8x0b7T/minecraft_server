426e1667263185335a6ae8329abab0c20280d483
n8x0b7T <deretilium@protonmail.com>
2021-08-29 21:57:26 -0400

    install important stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-server_jar=$(curl -s https://www.minecraft.net/en-us/download/server -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"· | grep 'aria-label="mincraft version"' |  sed -n 's/.*href="\([^"]*\).*/\1/p')
+server_jar=$(curl -s https://www.minecraft.net/en-us/download/server -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36" | grep 'aria-label="mincraft version"' |  sed -n 's/.*href="\([^"]*\).*/\1/p')
 
 echo "Downloading:"
 echo $server_jar
@@ -9,6 +9,6 @@ echo
 curl $server_jar -o server.jar
 
 sudo apt update
-sudo apt install screen
+sudo apt install screen openjdk-16-jre-headless
 
 chmod +x *.sh
